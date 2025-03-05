4dd527fceff733235189b5733d20a0d76c5a21d6
Caroolliny <caroline.rochadejesus07@gmail.com>
2025-03-05 16:57:14 -0300

    script criado

---
+++ aula3/script.js
@@ -0,0 +1,9 @@
+
+/*
+const botao = document.getElementById("botao");
+botao.style.backgroundColor = 'red';
+const nome = document.getElementById("nome");
+nome.value = 'thaila';*/
+
+const formulario = document.getElementById("Formulario");
+formulario.addEventListener("submit", function()())
